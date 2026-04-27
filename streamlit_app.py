b24bd369ff0e8a2b6d7d0a2ffe5889a45e0c02b2
david.chen@wavein.com.tw <david.chen@wavein.com.tw>
2026-04-27 18:51:21 +0800

    update vlm setting

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,9 +80,9 @@ def main():
     # 初始化 session_state
     if "vlm_config_snapshot" not in st.session_state:
         st.session_state.vlm_config_snapshot = VLMConfig(
-            api_base="http://192.168.66.26:40960/v1",
-            api_key="",
-            model="vlm",
+            api_base="http://llm-proxy:4000/v1",
+            api_key="abcd",
+            model="vlm-8b",
             prompt="Describe the scene in details in traditional chinese.",
             max_tokens=300,
         )
